3948edc6872d53b6d39ad1f0a02d4e6b0386202d
alotoftype <alotoftype@gmail.com>
2015-11-18 15:39:16 -0500

    issues with intializer files

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,1 @@
-heroku addons:add searchbox:starter
-heroku config:add ELASTICSEARCH_URL=`heroku config:get SEARCHBOX_URL`
 Searchkick.client = Elasticsearch::Client.new(hosts: ["localhost:9200", "localhost:9201"], retry_on_failure: true)
